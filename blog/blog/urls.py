8ec6a1ff7014c4e3c313b99c5c14cb487b0b9999
KryukovaDianaBFI2202 <mrs.diana.kruck@mail.ru>
2023-04-18 00:28:32 +0300

    first commit

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('archive/', views.archive),
     re_path(r'^article/(?P<article_id>\d+)$', views.get_article, name='get_article'),
+    path('article/new/', views.create_post),
     ]
 
